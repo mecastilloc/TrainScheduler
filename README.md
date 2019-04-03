8092c51471f6e89c1f1f25b28534271772196276
ManuCas <manucastle@hotmail.com>
2019-04-02 21:09:21 -0600

    final css style, away time formated in hh:mm

--- README.md
+++ README.md
@@ -3,6 +3,21 @@ Week 7 firebase basic homework
 
 
 
+
+## Features
+
+**[Live Demo](https://mecastilloc.github.io/TrainScheduler/index.html)**.
+
+When adding trains, administrators should be able to submit the following:
+Train Name
+Destination 
+First Train Time
+Frequency -- in minutes
+
+This app calculates when the next train will arrive; this should be relative to the current time.
+Users from many different machines are be able to view same train times.
+
+
 ## Commit History
 
 commit 1:
@@ -13,6 +28,9 @@ Initial file structure, html template, firebase connection, firebase read/write 
 
 commit 3:
 moment logic completed, all functions working
+
+comiit 4:
+final css style, away time formated in hh:mm
 
 
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//initial firebase app data
 var config = {
     apiKey: "AIzaSyCwxpb4tsDeDn1GrobbppFZdELY69Ajtlw",
     authDomain: "train-schedule-mecastilloc.firebaseapp.com",
@@ -10,17 +11,19 @@ var config = {
 var trainFirst;
 var away;
 var trainFrec;
-firebase.initializeApp(config);
 
+//initialize firebase app
+firebase.initializeApp(config);
+//firebase var
 var dB = firebase.database();
 
-$("#submitbtn").on("click", function(event) {
+
+$("#submitbtn").on("click", function (event) {
     event.preventDefault();
 
     var trainName = $("#trainname").val().trim();
     var destination = $("#traindestiny").val().trim();
     trainFirst = $("#trainfirst").val().trim();
-    console.log(trainFirst);
     trainFrec = $("#trainfrec").val().trim();
 
     dB.ref().push({
@@ -29,7 +32,7 @@ $("#submitbtn").on("click", function(event) {
         trainFirst: trainFirst,
         trainFrec: trainFrec,
     });
-    
+
 });
 
 // Firebase watcher + initial loader  .on("value")
@@ -41,48 +44,32 @@ dB.ref().on("child_added", function (snapshot) {
     var tdNext = $("<td>");
     var tdAway = $("<td>");
 
-       // Assumptions
-      
-
-       // Time is 3:30 AM
-       
-   
-       // First Time (pushed back 1 year to make sure it comes before current time)
-       var sv = snapshot.val();
-       
-       var firstTimeConverted = moment(sv.trainFirst, "HH:mm").subtract(1, "years");
-       console.log("trainfirst" + sv.trainFirst)
-       console.log("firdt time"+ firstTimeConverted);
-   
-       // Current Time
-       var currentTime = moment();
-       console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-   
-       // Difference between the times
-       away = moment().diff(moment(firstTimeConverted), "minutes");
-       console.log("DIFFERENCE IN TIME: " + away);
-   
-       // Time apart (remainder)
-       var tRemainder = away % sv.trainFrec;
-       console.log(tRemainder);
-   
-       // Minute Until Train
-       var tMinutesTillTrain = sv.trainFrec - tRemainder;
-       console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-   
-       // Next Train
-       var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-       console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
+    // this sv variable to write less code
+    var sv = snapshot.val();
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(sv.trainFirst, "hh:mm").subtract(1, "years");
+    console.log("trainfirst" + sv.trainFirst)
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // Difference between the times
+    away = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + away);
+    // Time apart (remainder)
+    var tRemainder = away % sv.trainFrec;
+    console.log(tRemainder);
+    // Minute Until Train
+    var tMinutesTillTrain = sv.trainFrec - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
     // Change the HTML to reflect
     tdName.text(sv.trainName);
     tdDestiny.text(sv.destination);
     tdFrec.text(sv.trainFrec);
-
-
-
     tdNext.text(moment(nextTrain).format("hh:mm"));
-    tdAway.text(tMinutesTillTrain);
+    tdAway.text(moment.duration(tMinutesTillTrain, "minutes").format("hh:mm"));
 
     tr.append(tdName);
     tr.append(tdDestiny);
